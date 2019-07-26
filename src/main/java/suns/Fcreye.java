6b894b9c59d6dec6ee17109fdf02e464acf1beaa
Administrator <Administrator@192.168.40.1>
2019-07-26 14:32:41 +0800

    init test

---
+++ src/main/java/suns/Fcreye.java
@@ -0,0 +1,19 @@
+package suns;
+
+public class Fcreye {
+	private String name = "licungang";
+	private String id = "李存刚";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<meta charset="utf8">
+</head>
+<body>
+<h2>wo shi li cun gang</h2>
+</body>
+</html>
